fcd659c9995c878e5d433c9733a0ce8c6878f907
Tuan Can <tuancv.dev@gmail.com>
2023-10-01 10:24:16 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ReactIcon from '@/assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <img src={ReactIcon}></img>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/__tests__/App.spec.tsx
@@ -0,0 +1,5 @@
+describe('App', () => {
+  it('should render App component without any issues', () => {
+    expect('').toBe('')
+  })
+})
